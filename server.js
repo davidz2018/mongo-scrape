05e986cf989b373f1abae75e1829c3510cbc6430
davidz2018 <dzhomebiz@gmail.com>
2018-06-16 13:22:01 -0700

    updated server file, models files, and public file

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var axios = require("axios")
 var cheerio = require("cheerio");
-var request = require("request");
-
-var db = require("./models");
+var MONGODB_URI = process.env.MONGODB_URI ||
+"mongodb://localhost"
+var db = require("./models")
 var PORT = 3000;
 
 var app = express();
@@ -18,5 +19,63 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.static("public"));
 
 mongoose.connect("mongoose://localhost/mongo-scrape");
+ 
+app.get("/scrape", function(req, res) {
+    axios.get("").then(function(reaponse) {
+        var $ = cheerio.load(response.data);
+        $("article h2").each(function(i, element) {
+            var result = {};
 
-app.get
+            result.title = $(this)
+                .children("a")
+                .text();
+            
+            db.Article.create(result)
+                .then(function(dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function(err) {
+                 return res.json(err)
+                });
+            });
+            res.send("Scrape Complete");
+    });
+});
+
+app.get("/articles", function(req, res) {
+    db.Article.find({})
+        .then(function(dbArticle) {
+            res.json(dbArticle)
+        })
+        .catch(function(err) {
+            res.json(err);
+        });
+});
+
+app.get("/articles/:id", function(req, res) {
+    db.Article.findOne({ _id: req.params.id })
+    .populate("note")
+    .then(function(dbArticle) {
+        res.json(dbArticle);
+    })
+    .catch(function(err) {
+        res.json(err);
+    });
+});
+
+app.post("/articles/:id", function(req, res) {
+    db.Note.create(req.body)
+      .then(function(dbNote) {
+          return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      })
+      .then(function(dbArticle) {
+          res.json(dbArticle);
+      })
+      .catch(function(err) {
+          res.json(err);
+      });
+});
+
+app.listen(PORT, function() {
+    console.log("App running on PORT " + PORT);
+});
